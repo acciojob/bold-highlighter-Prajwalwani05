5292ef898986fe750426bd208f8c19948befb9fc
Prajwal Wani <119412705+Prajwalwani05@users.noreply.github.com>
2023-05-22 14:27:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 function highlight() {
     //Write your code here
- let a = document.querySelectorAll('p strong');
-	a.forEach(element => {
-		element.style.color = 'green';
-	});
+ let a = document.getElementsByTagName('strong');
+	for(let x=0;x<a.length;x++){
+		a[x].style.color = 'green';
+	}
 
 }
 
 
 function return_normal() {
     //Write your code here
-  let b = document.querySelectAll('p strong');
-	b.forEach(element => {
-		element.style.color = 'black';
-	});
-    
+  let b = document.getElementsByTagName('strong');
+	for(let x=0;x<b.length;x++){
+		b[x].style.color = 'black';
+	}
 }
