75d2299dc745e528d6a8f50d31a75f479505787a
Prajwal Wani <119412705+Prajwalwani05@users.noreply.github.com>
2023-05-22 14:20:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function highlight() {
     //Write your code here
-
+ let a = document.querySelectorAll('p strong');
+	a.forEach(element => {
+		element.style.color = 'green';
+	});
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+  let b = document.querySelectAll('p strong');
+	b.forEach(element => {
+		element.style.color = 'black';
+	});
     
 }
